8e21faee87bd39b441c73ec20db1948172c09d3d
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-01-17 17:30:39 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import {notFound, errorHandler} from './middlewares';
+
+const app = express();
+
+(async () => {
+  try {
+    app.use(
+      helmet({
+        crossOriginEmbedderPolicy: false,
+        contentSecurityPolicy: false,
+      }),
+    );
+
+    app.use(notFound);
+    app.use(errorHandler);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+export default app;
